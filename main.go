9aa3f43eec6e012f0b32c6e3392cfab63cda22af
Cuk10 <oskar.cuk@gmail.com>
2025-07-12 19:33:31 +0200

    chirp

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -35,6 +36,53 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 }
 
+func handlerDecode(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if len(params.Body) > 140 {
+		err = fmt.Errorf("Chirp is too long")
+	}
+
+	if err != nil {
+		type returnVals struct {
+			Error string `json:"error"`
+		}
+
+		respBody := returnVals{
+			Error: fmt.Sprint(err),
+		}
+
+		data, _ := json.Marshal(respBody)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(data)
+
+	} else {
+		type returnVals struct {
+			Valid bool `json:"valid"`
+		}
+
+		respBody := returnVals{
+			Valid: true,
+		}
+
+		data, _ := json.Marshal(respBody)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(data)
+
+	}
+
+}
+
 func main() {
 
 	apiCfg := apiConfig{
@@ -52,6 +100,8 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
+	mux.HandleFunc("POST /api/validate_chirp", handlerDecode)
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
